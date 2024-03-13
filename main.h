070a73a7398697bd35c82595d9f358ba95036d8a
Zhang Yong <1930748422@qq.com>
2024-03-13 16:05:51 +0800

    Add files via upload
    Inlcudes file, put them in "include" dir of Platform IO Projects. 
    MCU -> EPS32 Dev Module
    Framework -> Arduino

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+extern  float left_motor_angle, left_motor_speed;
+extern  float right_motor_angle, right_motor_speed;
+
+
+
+#endif
---
+++ motor.h
@@ -0,0 +1,15 @@
+#ifndef  __MOTOR_H
+#define  __MOTOR_H
+
+#include <Arduino.h>
+
+
+void set_motor_speed(float left_speed, float right_speed);
+void read_left_motor();
+void read_right_motor();
+
+
+
+
+
+#endif
